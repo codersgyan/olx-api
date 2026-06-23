c33fb6fc0c302d2247095227ff4d7cfd4ab57026
Rakesh K <codersgyan@gmail.com>
2026-06-23 12:52:19 +0200

    add config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,29 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/codersgyan/olx-api/internal/config"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	cfg := config.MustLoad()
 
+	fmt.Println(("starting olx server..."))
+
+	mux := http.NewServeMux()
 	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-
 		w.Write([]byte(`{"status": "ok"}`))
 	})
 
 	srv := http.Server{
-		Addr:         ":8090",
+		Addr:         ":" + cfg.Port,
 		Handler:      mux,
 		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 30,
 		IdleTimeout:  time.Second * 60,
 	}
 
+	log.Printf("server is listening on %s", srv.Addr)
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("server failed: %v", err)
 	}
