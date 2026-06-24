f13b48a219e1c15da32b075b3a4099b226b6bc04
Rakesh K <codersgyan@gmail.com>
2026-06-24 16:51:16 +0200

    add migrate

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,12 +7,18 @@ import (
 	"time"
 
 	"github.com/codersgyan/olx-api/internal/config"
+	"github.com/codersgyan/olx-api/internal/db"
 	"github.com/codersgyan/olx-api/internal/handlers"
 )
 
 func main() {
 	cfg := config.MustLoad()
+	_, err := db.Connect(cfg.DatabaseUrl)
+	if err != nil {
+		log.Fatalf("main.db.connect: %v", err)
+	}
 
+	fmt.Println(("database connected"))
 	fmt.Println(("starting olx server..."))
 
 	mux := http.NewServeMux()
---
+++ cmd/migrate/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/codersgyan/olx-api/internal/config"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("usage: migrate <up | down>")
+	}
+
+	cfg := config.MustLoad()
+
+	m, err := migrate.New(
+		"file://migrations",
+		cfg.DatabaseUrl,
+	)
+	if err != nil {
+		log.Fatalf("migration.new: %v", err)
+	}
+
+	switch os.Args[1] {
+	case "up":
+		if err := m.Up(); err != nil {
+			log.Fatal(err)
+		}
+	case "down":
+		if err := m.Down(); err != nil {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatalf("unknown command: %s", os.Args[1])
+	}
+}
